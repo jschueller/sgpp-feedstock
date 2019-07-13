71cefb5ed1a3d22fd2ba84081582b52fda28c194
Julien Schueller <schueller@phimeca.com>
2019-07-13 18:50:39 +0200

    Enable osx (#1)
    * Enable osx
    
    * MNT: Re-rendered with conda-build 3.17.8, conda-smithy 3.4.0, and conda-forge-pinning 2019.07.04
    
    * debug
    
    * Set COMPILER
    
    * Add osx patch
    
    * Relink
    
    * Test examples

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,32 @@
 #!/bin/sh
 
-scons CC=${CC} CXX=${CXX} CPPFLAGS="-I${SP_DIR}/numpy/core/include" BOOST_INCLUDE_PATH=${PREFIX}/include GSL_INCLUDE_PATH=${PREFIX}/include SG_JAVA=0 COMPILE_BOOST_TESTS=0 RUN_PYTHON_TESTS=0 USE_ARMADILLO=0 USE_EIGEN=0 -j${CPU_COUNT} PREFIX=${PREFIX} -Q install
+if test `uname` = "Darwin"
+then
+  COMPILER=clang
+else
+  COMPILER=gnu
+fi
+export CXXFLAGS="${CXXFLAGS} -std=c++11"
+
+scons COMPILER=${COMPILER} CC=${CC} CXX=${CXX} CPPFLAGS="-I${SP_DIR}/numpy/core/include" BOOST_INCLUDE_PATH=${PREFIX}/include GSL_INCLUDE_PATH=${PREFIX}/include SG_JAVA=0 COMPILE_BOOST_TESTS=0 RUN_PYTHON_TESTS=0 USE_ARMADILLO=0 USE_EIGEN=0 -j${CPU_COUNT} PREFIX=${PREFIX} -Q install || cat config.log
 mv ${PREFIX}/lib/sgpp/* ${PREFIX}/lib
-cp -rLv lib/pysgpp ${SP_DIR}
+cp -RLv lib/pysgpp ${SP_DIR}
+
+if test `uname` = "Darwin"
+then
+  for mod in datadriven combigrid optimization pde solver quadrature base
+  do
+    install_name_tool -change ${mod}/libsgpp${mod}.dylib @rpath/libsgpp${mod}.dylib ${SP_DIR}/pysgpp/_pysgpp_swig.so
+    install_name_tool -change ${mod}/libsgpp${mod}.dylib @rpath/libsgpp${mod}.dylib ${PREFIX}/lib/libsgppdatadriven.dylib
+    install_name_tool -change ${mod}/libsgpp${mod}.dylib @rpath/libsgpp${mod}.dylib ${PREFIX}/lib/libsgppcombigrid.dylib
+    install_name_tool -change ${mod}/libsgpp${mod}.dylib @rpath/libsgpp${mod}.dylib ${PREFIX}/lib/libsgppdatadriven.dylib
+    install_name_tool -change ${mod}/libsgpp${mod}.dylib @rpath/libsgpp${mod}.dylib ${PREFIX}/lib/libsgppoptimization.dylib
+    install_name_tool -change ${mod}/libsgpp${mod}.dylib @rpath/libsgpp${mod}.dylib ${PREFIX}/lib/libsgpppde.dylib
+    install_name_tool -change ${mod}/libsgpp${mod}.dylib @rpath/libsgpp${mod}.dylib ${PREFIX}/lib/libsgppsolver.dylib
+    install_name_tool -change ${mod}/libsgpp${mod}.dylib @rpath/libsgpp${mod}.dylib ${PREFIX}/lib/libsgppquadrature.dylib
+    install_name_tool -change ${mod}/libsgpp${mod}.dylib @rpath/libsgpp${mod}.dylib ${PREFIX}/lib/libsgppbase.dylib
+  done
+fi
+
+python ./base/examples/quadrature.py
+python ./optimization/examples/optimization.py
